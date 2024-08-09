cd591d4fe4270ded747cbd92e78a6ad59b4223fd
Oscar Burgos <Oskyburgos81@gmail.com>
2024-08-09 13:08:07 -0500

    url vercel

--- api/server.js
+++ api/server.js
@@ -23,7 +23,7 @@ app.use('/api', router);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chess-knight-ecru.vercel.app/",
     method: ["GET", "POST"],
   }
 });
