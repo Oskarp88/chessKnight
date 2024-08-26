f20363c0a9ba4ef9e68b488243b751923b75032a
Oscar Burgos <Oskyburgos81@gmail.com>
2024-08-26 18:58:25 -0500

    friend mensaje chat

--- client/src/context/ChatContext.jsx
+++ client/src/context/ChatContext.jsx
@@ -1,10 +1,12 @@
 import { createContext, useCallback, useEffect, useState } from "react";
 import { getRequest, baseUrl, postRequest } from "../utils/services";
 import  io  from 'socket.io-client';
+import { useNavigate } from "react-router-dom";
 
 export const ChatContext = createContext();
 
 export const ChatContextProvider = ({children, user}) => {
+    const navigate = useNavigate();
     const [userChats, setUserchats] = useState(null);
     const [allUsers, setAllUsers] = useState([]);
     const [isUserChatsLoading, setIsUserChatsLoading] = useState(false);
@@ -190,6 +192,7 @@ export const ChatContextProvider = ({children, user}) => {
        //mark notification as read
        const mNotifications = notifications.map(el =>{
          if(n.senderId === el.senderId){
+            navigate('/auth/chat')
             return {...n, isRead: true}
          }else{
             return el
