4dbf62f438c997fc9df1568c1b17bfcda58e7cc1
Oscar Burgos <Oskyburgos81@gmail.com>
2024-08-30 15:28:07 -0500

    reset password alert

--- client/src/components/alerts/AlertDismissible.jsx
+++ client/src/components/alerts/AlertDismissible.jsx
@@ -1,24 +1,26 @@
 import { useState } from 'react';
 import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
-
+import style from './AlerDismissible.modulo.css'
 function AlertDismissible({title,text, show, setShow}) {
 
   return (
-    <>
-      <Alert show={show} variant="success">
-        <Alert.Heading>{title}</Alert.Heading>
-        <p>
-          {text}
-        </p>
-        <hr />
-        <div className="d-flex justify-content-end">
-          <Button onClick={() => setShow(false)} variant="outline-success">
-            Close me
-          </Button>
-        </div>
-      </Alert>
-    </>
+    <div className={style.modalOverlay}>
+      <div className={style.modalAlert}>
+        <Alert show={show} variant="success">
+            <Alert.Heading>{title}</Alert.Heading>
+            <p>
+            {text}
+            </p>
+            <hr />
+            <div className="d-flex justify-content-end">
+            <Button onClick={() => setShow(false)} variant="outline-success">
+                Close me
+            </Button>
+            </div>
+        </Alert>
+      </div>
+    </div>
   );
 }
 
