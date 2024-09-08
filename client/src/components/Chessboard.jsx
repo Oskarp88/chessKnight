6fb108263f2631dc806b1fec3525299314f38c5a
Oscar Burgos <Oskyburgos81@gmail.com>
2024-09-08 12:27:38 -0500

    componente checkmate deleted

--- client/src/components/Chessboard.jsx
+++ client/src/components/Chessboard.jsx
@@ -29,12 +29,11 @@ import ChatChess from './ChatChess';
 import BoardInfo from './board/BoardInfo';
 import RecordPlays from './board/RecordPlays';
 import PromotionPiece from './board/PromotionPiece';
-import { handleCheckMate } from '../handle/handleCheckMate';
 
 function Chessboard() {
 
   const {
-     socket,  
+     socket, 
      room, 
      setRoom, 
      infUser, 
@@ -332,22 +331,57 @@ useEffect(()=>{
   useEffect(() => {
     if (whiteTime === 0 || blackTime === 0) {
       if (isWhiteTime === infUser?.color) {
-        
-        handleCheckMate(
-          infUser,
-          auth,
-          userChess,
-          setCheckMate,
-          'derrota'
-        );       
+        setCheckMate((prevCheckMate) => ({
+          ...prevCheckMate,
+          userId: auth?.user?._id,
+          opponentId: infUser?.idOpponent,
+          name: auth?.user?.username,
+          nameOpponent: infUser?.username,
+          bandera: auth?.user?.imagenBandera,
+          banderaOpponent: infUser?.bandera,
+          country: auth?.user?.country,
+          countryOpponent: infUser?.country,
+          time: `${infUser?.time === 60 || infUser?.time === 120 ? 'bullet' :
+            infUser?.time === 180 || infUser?.time === 300 ? 'blitz' :
+              'fast'}`,
+          game: 'derrota',
+          eloUser: `${infUser?.time === 60 || infUser?.time === 120 ? parseInt(userChess?.eloBullet) :
+            infUser?.time === 180 || infUser?.time === 300 ? parseInt(userChess?.eloBlitz) :
+            parseInt(userChess?.eloFast)}`,
+          eloOpponent: `${infUser?.time === 60 || infUser?.time === 120 ? parseInt(infUser?.bullet) :
+            infUser?.time === 180 || infUser?.time === 300 ?  parseInt(infUser?.blitz) :
+            parseInt(infUser?.fast)}`,
+          elo: `${infUser?.time === 60 || infUser?.time === 120 ? parseInt(userChess?.eloBullet) - parseInt(infUser?.bullet) :
+            infUser?.time === 180 || infUser?.time === 300 ? parseInt(userChess?.eloBlitz) - parseInt(infUser?.blitz) :
+            parseInt(userChess?.eloFast) - parseInt(infUser?.fast)}`,
+          color: infUser?.color
+        }));
       } else {
-        handleCheckMate(
-          infUser,
-          auth,
-          userChess,
-          setCheckMate,
-          'victoria'
-        ); 
+        setCheckMate((prevCheckMate) => ({
+          ...prevCheckMate,
+          userId: auth?.user?._id,
+          opponentId: infUser?.idOpponent,
+          name: auth?.user?.username,
+          nameOpponent: infUser?.username,
+          bandera: auth?.user?.imagenBandera,
+          banderaOpponent: infUser?.bandera,
+          country: auth?.user?.country,
+          countryOpponent: infUser?.country,
+          time: `${infUser?.time === 60 || infUser?.time === 120 ? 'bullet' :
+            infUser?.time === 180 || infUser?.time === 300 ? 'blitz' :
+              'fast'}`,
+          game: 'victoria',
+          eloUser: `${infUser?.time === 60 || infUser?.time === 120 ? parseInt(userChess?.eloBullet) :
+            infUser?.time === 180 || infUser?.time === 300 ? parseInt(userChess?.eloBlitz) :
+            parseInt(userChess?.eloFast)}`,
+          eloOpponent: `${infUser?.time === 60 || infUser?.time === 120 ? parseInt(infUser?.bullet) :
+            infUser?.time === 180 || infUser?.time === 300 ?  parseInt(infUser?.blitz) :
+            parseInt(infUser?.fast)}`,
+          elo: `${infUser?.time === 60 || infUser?.time === 120 ? parseInt(userChess?.eloBullet) - parseInt(infUser?.bullet) :
+            infUser?.time === 180 || infUser?.time === 300 ? parseInt(userChess?.eloBlitz) - parseInt(infUser?.blitz) :
+            parseInt(userChess?.eloFast) - parseInt(infUser?.fast)}`,
+            color: infUser?.color
+        }));
       }
     }
   }, [isWhiteTime]);
