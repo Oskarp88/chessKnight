031af669cef10aa0182f4c62b40b13a4eebb1c40
Oscar Burgos <Oskyburgos81@gmail.com>
2024-09-10 12:42:23 -0500

    actualizado los valores de cada rango

--- client/src/components/insignias/Insignias.jsx
+++ client/src/components/insignias/Insignias.jsx
@@ -1,31 +1,40 @@
-import { Aprendiz, Avanzado, Competente, Experto, Maestro, Novato, Veterano } from '../../img'
+import { Aprendiz, Avanzado, Competente, Elite, Experto, Leyenda, Maestro, Novato, Prodigio, Veterano } from '../../img'
 
 function Insignias({o, time}) {
   return (
     <div>
         { time === 60 || time === 120 ? 
-            o?.eloBullet < 21 ? <Novato /> : 
-            o?.eloBullet >= 21 && o?.eloBullet <= 61 ? <Aprendiz/> : 
-            o?.eloBullet >= 62 && o?.eloBullet <= 100 ? <Avanzado /> : 
-            o?.eloBullet >= 101 && o?.eloBullet <= 230 ? <Competente /> :
-            o?.eloBullet >= 231 && o?.eloBullet <= 390 ? <Experto /> :
-            o?.eloBullet >= 391 && o?.eloBullet <= 549 ? <Veterano /> : 
-            <Maestro/> :
+            o?.eloBullet < 50 ? <Novato /> : 
+            o?.eloBullet >= 50 && o?.eloBullet <= 150 ? <Aprendiz/> : 
+            o?.eloBullet >= 151 && o?.eloBullet <= 300? <Avanzado /> : 
+            o?.eloBullet >= 301 && o?.eloBullet <= 439 ? <Competente /> :
+            o?.eloBullet >= 440 && o?.eloBullet <= 515 ? <Experto /> :
+            o?.eloBullet >= 516 && o?.eloBullet <= 705 ? <Veterano /> : 
+            o?.eloBullet >= 706 && o?.eloBullet  <= 850 ? <Maestro/> : 
+            o?.eloBullet >= 851 && o?.eloBullet  <= 999 ? <Elite/> :
+            o?.eloBullet >= 1000 && o?.eloBullet <= 1359 ? <Prodigio /> : 
+             <Leyenda/> :
             time === 180 || time === 300 ? 
-            o?.eloBlitz < 21 ? <Novato/> : 
-            o?.eloBlitz >= 21 && o?.eloBlitz <= 61 ? <Aprendiz/> : 
-            o?.eloBlitz >= 62 && o?.eloBlitz <= 100 ? <Avanzado/> : 
-            o?.eloBlitz >= 101 && o?.eloBlitz <= 230 ? <Competente/> :
-            o?.eloBlitz >= 231 && o?.eloBlitz <= 390 ? <Experto/> : 
-            o?.eloBlitz >= 391 && o?.eloBlitz <= 549 ?  <Veterano/> : 
-            <Maestro/> : 
-            o?.eloFast < 21 ? <Novato/> : 
-            o?.eloFast >= 21 && o?.eloFast <= 61 ? <Aprendiz/> : 
-            o?.eloFast  >= 62 && o?.eloFast  <= 100 ? <Avanzado/> : 
-            o?.eloFast  >= 101 && o?.eloFast  <= 230 ? <Competente/> :
-            o?.eloFast  >= 231 && o?.eloFast  <= 390 ? <Experto/> : 
-            o?.eloFast  >= 391 && o?.eloFast  <= 549 ? <Veterano/> :
-            <Maestro/>
+            o?.eloBlitz < 50 ? <Novato /> : 
+            o?.eloBlitz >= 50 && o?.eloBlitz <= 150 ? <Aprendiz/> : 
+            o?.eloBlitz >= 151 && o?.eloBlitz <= 300? <Avanzado /> : 
+            o?.eloBlitz >= 301 && o?.eloBlitz <= 439 ? <Competente /> :
+            o?.eloBlitz >= 440 && o?.eloBlitz <= 515 ? <Experto /> :
+            o?.eloBlitz >= 516 && o?.eloBlitz <= 705 ? <Veterano /> : 
+            o?.eloBlitz >= 706 && o?.eloBlitz  <= 850 ? <Maestro/> : 
+            o?.eloBlitz >= 851 && o?.eloBlitz  <= 999 ? <Elite/> :
+            o?.eloBlitz >= 1000 && o?.eloBlitz <= 1359 ? <Prodigio /> : 
+             <Leyenda/> :
+             o?.eloFast  < 50 ? <Novato /> : 
+             o?.eloFast >= 50 && o?.eloFast <= 150 ? <Aprendiz/> : 
+             o?.eloFast >= 151 && o?.eloFast <= 300? <Avanzado /> : 
+             o?.eloFast >= 301 && o?.eloFast <= 439 ? <Competente /> :
+             o?.eloFast >= 440 && o?.eloFast <= 515 ? <Experto /> :
+             o?.eloFast >= 516 && o?.eloFast <= 705 ? <Veterano /> : 
+             o?.eloFast >= 706 && o?.eloFast  <= 850 ? <Maestro/> : 
+             o?.eloFast >= 851 && o?.eloFast <= 999 ? <Elite/> :
+             o?.eloFast >= 1000 && o?.eloFast <= 1359 ? <Prodigio /> : 
+             <Leyenda/> 
          } 
     </div>
   )
