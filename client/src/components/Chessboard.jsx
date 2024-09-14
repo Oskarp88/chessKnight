d329c050afb06ae59601fb1f960ccd3669e3b177
Oscar Burgos <Oskyburgos81@gmail.com>
2024-09-14 10:50:17 -0500

    bug check

--- client/src/components/Chessboard.jsx
+++ client/src/components/Chessboard.jsx
@@ -1056,11 +1056,11 @@ useEffect(()=>{
           // Implementar la lógica para manejar el jaque mate
           console.log('¡estas en jake'); 
           document.removeEventListener('mousemove', onMouseMove);
+          document.removeEventListener('mouseup', onMouseUp);
           pieceElement.style.position = '';
           pieceElement.style.left = '';
-          pieceElement.style.top = '';
-            
-          setSelectedPiece(null)    
+          pieceElement.style.top = '';  
+          setSelectedPiece(null)     
           return;
         } 
 
