79db465577760003a8d5789bdccb38a1b5c414b2
Oscar Burgos <Oskyburgos81@gmail.com>
2024-09-20 12:52:31 -0500

    casillas jugadas

--- client/src/components/Chessboard.jsx
+++ client/src/components/Chessboard.jsx
@@ -620,16 +620,19 @@ useEffect(()=>{
   const handleOpponentMove = async (data) => {
     const { piece, x, y, turn, pieces} = data;
       setCurrentTurn(turn);
-      setPieces((prevPieces) => 
-      prevPieces.map((prevPiece) => {
-        const newPiece = pieces.find((p) => p.type === prevPiece.type && p.color === prevPiece.color);
-        if (newPiece) {
-          // Solo actualizamos las coordenadas x y y, manteniendo el resto de propiedades
-          return { ...prevPiece, x: newPiece.x, y: newPiece.y };
-        }
-        return prevPiece; // Si no encuentra la pieza equivalente, no hace cambios
-      })
-    );
+      setPieces((prevPieces) => {
+        return pieces.map((newPiece) => {
+            const prevPiece = prevPieces.find(p => p.x === newPiece.x && p.y === newPiece.y && p.color === newPiece.color);
+            
+            if (prevPiece) {
+                // Mantener la imagen existente y actualizar el resto de propiedades
+                return { newPiece, image: prevPiece.image };
+            }
+
+            // Si no hay pieza previa, usar las propiedades tal cual (incluyendo image de newPiece)
+            return newPiece;
+        });
+    });
 
       setStartCell(null)
       setDestinationCell(null);
