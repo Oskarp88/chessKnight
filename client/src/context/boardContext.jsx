d6eb73bdc8d97f641df5c5ad9e46b67c4e5d98b5
Oscar Burgos <Oskyburgos81@gmail.com>
2024-09-27 17:20:42 -0500

    chessboard localstore en usecontext

--- client/src/context/boardContext.jsx
+++ client/src/context/boardContext.jsx
@@ -283,7 +283,7 @@ export const ChessboardProvider = ({ children }) => {
     const minutes = Math.floor(time / 60).toString().padStart(2, "0");
     const seconds = (time % 60).toString().padStart(2, "0");
     return `${minutes}:${seconds}`;
-  });
+  }, []);
   
   useEffect(() => {
 
